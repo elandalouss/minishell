173a5ff86de3fd6432ab24d65313529572a61087
elandalouss <ayoub.el.andaloussi02@gmail.com>
2022-10-17 23:05:23 +0100

    wa33333

--- minishell/main.c
+++ minishell/main.c
@@ -47,23 +47,23 @@ int	main(int ac, char **av, char **env)
 		cmds_line = parsing_part(str);
 		if (g_codes.g_error_code != 0)
 			continue ;// I GUESS NO NEED TO CHECK IF CMDS_LINE IS null
-	// t_cmd *temp;
-	// temp = cmds_line;
-	// int i = 0;
-	// while (temp)
-	// {
-	// 	printf("========================\n");
-	// 	printf("index is        %d \n", temp->index);
-	// 	printf("flag is         %d \n", temp->flag);
-	// 	printf("in_file fd is 	%d \n", temp->in_file_fd);
-	// 	printf("out_file fd is	%d \n", temp->out_file_fd);
-	// 	i = 0;
-	// 	printf("av is : \n");
-	// 	while (temp->av && temp->av[i])
-	// 		printf("|%s|\n", temp->av[i++]);
-	// 	printf("========================\n");
-	// 	temp = temp->next;
-	// }
+	t_cmd *temp;
+	temp = cmds_line;
+	int i = 0;
+	while (temp)
+	{
+		printf("========================\n");
+		printf("index is        %d \n", temp->index);
+		printf("flag is         %d \n", temp->flag);
+		printf("in_file fd is 	%d \n", temp->in_file_fd);
+		printf("out_file fd is	%d \n", temp->out_file_fd);
+		i = 0;
+		printf("av is : \n");
+		while (temp->av && temp->av[i])
+			printf("|%s|\n", temp->av[i++]);
+		printf("========================\n");
+		temp = temp->next;
+	}
 		// printf("je suis entrain d'excuter :P\n");
 		//==============================================>handl the "" case, this message for me not you
 		execution_part(cmds_line);
